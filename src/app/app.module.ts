583c892c1ae340567fdf7f36dbbd77c3b0b6c787
João Pimentel <joao.pimentel@watermelontecnologia.com>
2021-07-28 16:22:15 -0300

    Assignment 3 complete

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { UsernameComponent } from './username/username.component';
+import { AssignmentThreeComponent } from './assignment-three/assignment-three.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     UsernameComponent,
+    AssignmentThreeComponent,
   ],
   imports: [
     BrowserModule,
