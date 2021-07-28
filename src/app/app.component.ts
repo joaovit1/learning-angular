b9463894345bb4000f157df0993ed9b60a8a5692
João Pimentel <joao.pimentel@watermelontecnologia.com>
2021-07-28 13:59:00 -0300

    Assignment 2 complete

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,21 +6,5 @@ import { Component } from '@angular/core';
 	styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-	title = "I'm in the";
-	appName = 'LearningApp'
-	buttonDisable = true;
-
-	constructor(){
-		setTimeout(()=> {
-			this.buttonDisable = false;
-		}, 2000)
-	}
-
-	onButtonChangeTitle(){
-		this.title= "I'm in the " + this.appName +'!'
-	}
-
-	onInputUpdateAppName(event: any){
-		this.appName = event.target.value
-	}
+	title = "I'm in the LearningApp";
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
-import { WarningAlertComponent } from './warning-alert/warning-alert.component';
-import { SuccessAlertComponent } from './success-alert/success-alert.component';
-import { ServerComponent } from './server/server.component';
+import { UsernameComponent } from './username/username.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    WarningAlertComponent,
-    SuccessAlertComponent,
-    ServerComponent,
+    UsernameComponent,
   ],
   imports: [
     BrowserModule,
