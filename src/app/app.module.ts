85665ebe009a6d87b9cf4304bd085f8961088b30
João Pimentel <joao.pimentel@watermelontecnologia.com>
2021-07-29 10:31:05 -0300

    Preparing to start course project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
-import { UsernameComponent } from './username/username.component';
-import { AssignmentThreeComponent } from './assignment-three/assignment-three.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    UsernameComponent,
-    AssignmentThreeComponent,
   ],
   imports: [
     BrowserModule,
